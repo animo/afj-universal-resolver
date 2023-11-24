fcb2359daef36b075433e0a9ba0c32fb158c7cbe
Berend Sliedrecht <sliedrecht@berend.io>
2023-11-24 12:40:17 +0100

    included tests and minor linting
    Signed-off-by: Berend Sliedrecht <sliedrecht@berend.io>

--- src/UniversalDidResovler.ts
+++ src/UniversalDidResovler.ts
@@ -3,51 +3,50 @@ import {
   AgentContext,
   AgentDependencies,
   DidResolutionResult,
-  DidResolver,
-} from "@aries-framework/core";
+  DidResolver
+} from "@aries-framework/core"
 
 export type UniversalDidResolverOptions = {
-  baseUrl: string;
-  supportedMethods: Array<string>;
-};
+  baseUrl: string
+  supportedMethods: Array<string>
+}
 
 export class UniversalDidResolver implements DidResolver {
-  public supportedMethods: Array<string>;
-  private url: string;
+  public supportedMethods: Array<string>
+  private url: string
 
   public constructor({
     supportedMethods,
-    baseUrl,
+    baseUrl
   }: UniversalDidResolverOptions) {
-    this.supportedMethods = supportedMethods;
-    this.url = `${baseUrl}/1.0/identifiers`;
+    this.supportedMethods = supportedMethods
+    this.url = `${baseUrl}/1.0/identifiers`
   }
 
   public static async initializeWithDynamicMethods(
     agentDependencies: AgentDependencies,
     baseUrl: string
   ) {
-    const methodsUrl = `${baseUrl}/1.0/methods`;
+    const methodsUrl = `${baseUrl}/1.0/methods`
 
     const supportedMethods = await (
       await agentDependencies.fetch(methodsUrl)
-    ).json();
+    ).json()
 
     return new UniversalDidResolver({
       baseUrl,
-      supportedMethods: supportedMethods,
-    });
+      supportedMethods: supportedMethods
+    })
   }
 
   public async resolve(
     agentContext: AgentContext,
     did: string
   ): Promise<DidResolutionResult> {
-    const requestUrl = `${this.url}/${did}`;
+    const requestUrl = `${this.url}/${did}`
 
-    const result =
-      await agentContext.config.agentDependencies.fetch(requestUrl);
+    const result = await agentContext.config.agentDependencies.fetch(requestUrl)
 
-    return (await result.json()) as DidResolutionResult;
+    return (await result.json()) as DidResolutionResult
   }
 }
---
+++ tests/e2e.test.ts
@@ -0,0 +1,86 @@
+import { describe, it } from "node:test"
+import assert, { deepStrictEqual } from "node:assert"
+
+import { UniversalDidResolver } from "../src"
+import { agentDependencies } from "@aries-framework/node"
+import { Agent, DidsModule } from "@aries-framework/core"
+import { AskarModule } from "@aries-framework/askar"
+import { ariesAskar } from "@hyperledger/aries-askar-nodejs"
+
+describe("Universal Resolver + AFJ", async () => {
+  it("should instantiate a universal resolver", async () => {
+    const uni = new UniversalDidResolver({
+      baseUrl: "https://dev.uniresolver.io",
+      supportedMethods: ["key"]
+    })
+    assert(uni instanceof UniversalDidResolver)
+  })
+
+  it("should instantiate a universal resolver with dynamic methods", async () => {
+    const uni = await UniversalDidResolver.initializeWithDynamicMethods(
+      agentDependencies,
+      "https://dev.uniresolver.io"
+    )
+    assert(uni instanceof UniversalDidResolver)
+  })
+
+  it("should use the uniresolver via the agent", async () => {
+    const devUniResolverUrl = "https://dev.uniresolver.io"
+    const didKey =
+      "did:key:z4MXj1wBzi9jUstyPMS4jQqB6KdJaiatPkAtVtGc6bQEQEEsKTic4G7Rou3iBf9vPmT5dbkm9qsZsuVNjq8HCuW1w24nhBFGkRE4cd2Uf2tfrB3N7h4mnyPp1BF3ZttHTYv3DLUPi1zMdkULiow3M1GfXkoC6DoxDUm1jmN6GBj22SjVsr6dxezRVQc7aj9TxE7JLbMH1wh5X3kA58H3DFW8rnYMakFGbca5CB2Jf6CnGQZmL7o5uJAdTwXfy2iiiyPxXEGerMhHwhjTA1mKYobyk2CpeEcmvynADfNZ5MBvcCS7m3XkFCMNUYBS9NQ3fze6vMSUPsNa6GVYmKx2x6JrdEjCk3qRMMmyjnjCMfR4pXbRMZa3i"
+
+    const uni = await UniversalDidResolver.initializeWithDynamicMethods(
+      agentDependencies,
+      devUniResolverUrl
+    )
+
+    const agent = new Agent({
+      config: {
+        label: "my-agent",
+        walletConfig: { id: "some-id", key: "some-key" }
+      },
+      modules: {
+        askar: new AskarModule({ ariesAskar }),
+        dids: new DidsModule({ resolvers: [uni] })
+      },
+      dependencies: agentDependencies
+    })
+
+    await agent.initialize()
+
+    const { didDocument } = await agent.dids.resolve(didKey)
+
+    deepStrictEqual(didDocument, {
+      "@context": [
+        "https://www.w3.org/ns/did/v1",
+        {
+          JsonWebKey2020: "https://w3id.org/security#JsonWebKey2020",
+          publicKeyJwk: {
+            "@id": "https://w3id.org/security#publicKeyJwk",
+            "@type": "@json"
+          }
+        }
+      ],
+      id: "did:key:z4MXj1wBzi9jUstyPMS4jQqB6KdJaiatPkAtVtGc6bQEQEEsKTic4G7Rou3iBf9vPmT5dbkm9qsZsuVNjq8HCuW1w24nhBFGkRE4cd2Uf2tfrB3N7h4mnyPp1BF3ZttHTYv3DLUPi1zMdkULiow3M1GfXkoC6DoxDUm1jmN6GBj22SjVsr6dxezRVQc7aj9TxE7JLbMH1wh5X3kA58H3DFW8rnYMakFGbca5CB2Jf6CnGQZmL7o5uJAdTwXfy2iiiyPxXEGerMhHwhjTA1mKYobyk2CpeEcmvynADfNZ5MBvcCS7m3XkFCMNUYBS9NQ3fze6vMSUPsNa6GVYmKx2x6JrdEjCk3qRMMmyjnjCMfR4pXbRMZa3i",
+      verificationMethod: [
+        {
+          id: "did:key:z4MXj1wBzi9jUstyPMS4jQqB6KdJaiatPkAtVtGc6bQEQEEsKTic4G7Rou3iBf9vPmT5dbkm9qsZsuVNjq8HCuW1w24nhBFGkRE4cd2Uf2tfrB3N7h4mnyPp1BF3ZttHTYv3DLUPi1zMdkULiow3M1GfXkoC6DoxDUm1jmN6GBj22SjVsr6dxezRVQc7aj9TxE7JLbMH1wh5X3kA58H3DFW8rnYMakFGbca5CB2Jf6CnGQZmL7o5uJAdTwXfy2iiiyPxXEGerMhHwhjTA1mKYobyk2CpeEcmvynADfNZ5MBvcCS7m3XkFCMNUYBS9NQ3fze6vMSUPsNa6GVYmKx2x6JrdEjCk3qRMMmyjnjCMfR4pXbRMZa3i#z4MXj1wBzi9jUstyPMS4jQqB6KdJaiatPkAtVtGc6bQEQEEsKTic4G7Rou3iBf9vPmT5dbkm9qsZsuVNjq8HCuW1w24nhBFGkRE4cd2Uf2tfrB3N7h4mnyPp1BF3ZttHTYv3DLUPi1zMdkULiow3M1GfXkoC6DoxDUm1jmN6GBj22SjVsr6dxezRVQc7aj9TxE7JLbMH1wh5X3kA58H3DFW8rnYMakFGbca5CB2Jf6CnGQZmL7o5uJAdTwXfy2iiiyPxXEGerMhHwhjTA1mKYobyk2CpeEcmvynADfNZ5MBvcCS7m3XkFCMNUYBS9NQ3fze6vMSUPsNa6GVYmKx2x6JrdEjCk3qRMMmyjnjCMfR4pXbRMZa3i",
+          type: "JsonWebKey2020",
+          controller:
+            "did:key:z4MXj1wBzi9jUstyPMS4jQqB6KdJaiatPkAtVtGc6bQEQEEsKTic4G7Rou3iBf9vPmT5dbkm9qsZsuVNjq8HCuW1w24nhBFGkRE4cd2Uf2tfrB3N7h4mnyPp1BF3ZttHTYv3DLUPi1zMdkULiow3M1GfXkoC6DoxDUm1jmN6GBj22SjVsr6dxezRVQc7aj9TxE7JLbMH1wh5X3kA58H3DFW8rnYMakFGbca5CB2Jf6CnGQZmL7o5uJAdTwXfy2iiiyPxXEGerMhHwhjTA1mKYobyk2CpeEcmvynADfNZ5MBvcCS7m3XkFCMNUYBS9NQ3fze6vMSUPsNa6GVYmKx2x6JrdEjCk3qRMMmyjnjCMfR4pXbRMZa3i",
+          publicKeyJwk: {
+            kty: "RSA",
+            n: "sbX82NTV6IylxCh7MfV4hlyvaniCajuP97GyOqSvTmoEdBOflFvZ06kR_9D6ctt45Fk6hskfnag2GG69NALVH2o4RCR6tQiLRpKcMRtDYE_thEmfBvDzm_VVkOIYfxu-Ipuo9J_S5XDNDjczx2v-3oDh5-CIHkU46hvFeCvpUS-L8TJSbgX0kjVk_m4eIb9wh63rtmD6Uz_KBtCo5mmR4TEtcLZKYdqMp3wCjN-TlgHiz_4oVXWbHUefCEe8rFnX1iQnpDHU49_SaXQoud1jCaexFn25n-Aa8f8bc5Vm-5SeRwidHa6ErvEhTvf1dz6GoNPp2iRvm-wJ1gxwWJEYPQ",
+            e: "AQAB"
+          }
+        }
+      ],
+      authentication: [
+        "did:key:z4MXj1wBzi9jUstyPMS4jQqB6KdJaiatPkAtVtGc6bQEQEEsKTic4G7Rou3iBf9vPmT5dbkm9qsZsuVNjq8HCuW1w24nhBFGkRE4cd2Uf2tfrB3N7h4mnyPp1BF3ZttHTYv3DLUPi1zMdkULiow3M1GfXkoC6DoxDUm1jmN6GBj22SjVsr6dxezRVQc7aj9TxE7JLbMH1wh5X3kA58H3DFW8rnYMakFGbca5CB2Jf6CnGQZmL7o5uJAdTwXfy2iiiyPxXEGerMhHwhjTA1mKYobyk2CpeEcmvynADfNZ5MBvcCS7m3XkFCMNUYBS9NQ3fze6vMSUPsNa6GVYmKx2x6JrdEjCk3qRMMmyjnjCMfR4pXbRMZa3i#z4MXj1wBzi9jUstyPMS4jQqB6KdJaiatPkAtVtGc6bQEQEEsKTic4G7Rou3iBf9vPmT5dbkm9qsZsuVNjq8HCuW1w24nhBFGkRE4cd2Uf2tfrB3N7h4mnyPp1BF3ZttHTYv3DLUPi1zMdkULiow3M1GfXkoC6DoxDUm1jmN6GBj22SjVsr6dxezRVQc7aj9TxE7JLbMH1wh5X3kA58H3DFW8rnYMakFGbca5CB2Jf6CnGQZmL7o5uJAdTwXfy2iiiyPxXEGerMhHwhjTA1mKYobyk2CpeEcmvynADfNZ5MBvcCS7m3XkFCMNUYBS9NQ3fze6vMSUPsNa6GVYmKx2x6JrdEjCk3qRMMmyjnjCMfR4pXbRMZa3i"
+      ],
+      assertionMethod: [
+        "did:key:z4MXj1wBzi9jUstyPMS4jQqB6KdJaiatPkAtVtGc6bQEQEEsKTic4G7Rou3iBf9vPmT5dbkm9qsZsuVNjq8HCuW1w24nhBFGkRE4cd2Uf2tfrB3N7h4mnyPp1BF3ZttHTYv3DLUPi1zMdkULiow3M1GfXkoC6DoxDUm1jmN6GBj22SjVsr6dxezRVQc7aj9TxE7JLbMH1wh5X3kA58H3DFW8rnYMakFGbca5CB2Jf6CnGQZmL7o5uJAdTwXfy2iiiyPxXEGerMhHwhjTA1mKYobyk2CpeEcmvynADfNZ5MBvcCS7m3XkFCMNUYBS9NQ3fze6vMSUPsNa6GVYmKx2x6JrdEjCk3qRMMmyjnjCMfR4pXbRMZa3i#z4MXj1wBzi9jUstyPMS4jQqB6KdJaiatPkAtVtGc6bQEQEEsKTic4G7Rou3iBf9vPmT5dbkm9qsZsuVNjq8HCuW1w24nhBFGkRE4cd2Uf2tfrB3N7h4mnyPp1BF3ZttHTYv3DLUPi1zMdkULiow3M1GfXkoC6DoxDUm1jmN6GBj22SjVsr6dxezRVQc7aj9TxE7JLbMH1wh5X3kA58H3DFW8rnYMakFGbca5CB2Jf6CnGQZmL7o5uJAdTwXfy2iiiyPxXEGerMhHwhjTA1mKYobyk2CpeEcmvynADfNZ5MBvcCS7m3XkFCMNUYBS9NQ3fze6vMSUPsNa6GVYmKx2x6JrdEjCk3qRMMmyjnjCMfR4pXbRMZa3i"
+      ]
+    })
+  })
+})
